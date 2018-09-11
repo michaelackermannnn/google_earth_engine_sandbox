f15ee26bde637108f3501df8f258ec54d9548940
Michael Ackermann <michaelackermannnn@gmail.com>
2018-09-11 21:28:53 +0200

    Print url to image download

--- src/test.py
+++ src/test.py
@@ -7,3 +7,12 @@ ee.Initialize()
 # Print the information for an image asset.
 image = ee.Image('srtm90_v4')
 print(image.getInfo())
+
+# Print image download link
+path = image.getDownloadUrl({
+    'scale': 30,
+    'crs': 'EPSG:4326',
+    'region': '[[-120, 35], [-119, 35], [-119, 34], [-120, 34]]'
+})
+
+print(path)
